f78a8506e27fb2e7b2068ee67bfd8ffd1a406a6d
Guillaume Gagnon <ggqc007@gmail.com>
2014-02-15 19:02:50 -0500

    Travail sur Amizone, fait methode rechercherProduitsParMotCle()

--- src/Amizone.java
+++ src/Amizone.java
@@ -166,8 +166,43 @@ public class Amizone {
         }
     }
 
+    /**
+     * Cette methode retourne une liste de tous les produits (parmi les produits
+     * vendus par tous les fournisseurs) dont la description contient le mot cle
+     * donne. Pour simplifier la recherche, nous supposerons ici que la description
+     * des produits est une suite de mots separes uniquement par un ou des espaces
+     * (aucun signe de ponctuation) et que le mot cle donne est un seul mot, sans
+     * espace. La recherche ne prend pas compte de la casse. Les produits retournes
+     * ont une quantite en stock strictement plus grande que 0. Un meme produit 
+     * peut revenir plusieurs fois dans la liste retournee si celui-ci est vendu
+     * par differents fournisseurs (et qu’il correspond à la recherche, evidemment).
+     * Si aucun produit n’est trouve, la methode retourne une liste vide.
+     * 
+     * @param motCle Mot cle recherche.
+     * @return Liste de Produit ou liste vide si aucun produit n'est trouve.
+     */
     public ArrayList<Produit> rechercherProduitsParMotCle(String motCle) {
-        return null; //A FAIRE
+        ArrayList<Produit> resultatRech = new ArrayList<Produit>();
+        Produit[] produitsFournisseurs;
+        
+        // Recherche les listes de produits de tous les fournisseurs
+        for (int i = 0; i < utilisateurs.size(); i++) {
+            if (utilisateurs.get(i) instanceof Fournisseur) {
+                produitsFournisseurs = ((Fournisseur)utilisateurs.get(i)).getProduits();
+                // Recherche dans la liste d'un fournisseur
+                for (int j = 0; j < produitsFournisseurs.length; j++) {
+                    if (produitsFournisseurs[j] instanceof Produit 
+                            && rechStringDansDescriptionProduit(produitsFournisseurs[j], motCle) 
+                            && produitsFournisseurs[j].getQuantite() > 0) {
+                        // Ajoute le produit!
+                        resultatRech.add(produitsFournisseurs[j]);
+                    }
+                }
+            }
+        }
+        
+        
+        return resultatRech;
     }
 
     public ArrayList<Fournisseur> rechercherFournisseurParEvaluation(int codeProduit) {
@@ -190,8 +225,8 @@ public class Amizone {
      * METHODES PRIVEE
      */
     /**
-     * Verifie la liste d'utilisateurs pour savoir si le id de l'utilisateur en
-     * parametre est deja present.
+     * Methode utilitaire qui la liste d'utilisateurs pour savoir si le id de 
+     * l'utilisateur en parametre est deja present.
      *
      * @param utilisateur Utilisateur a verifier.
      * @return Vrai si utilisateur est deja present.
@@ -207,7 +242,8 @@ public class Amizone {
     }
 
     /**
-     * Verifie si un fournisseur a un produit en stock a vendre (quantite > 0).
+     * Methode utilitaire qui verifie si un fournisseur a un produit en stock a 
+     * vendre (quantite > 0).
      *
      * @param fournisseur Le fournisseur (Utilisateur) qui desire s'inscrire.
      * @return Vrai si l'utilisateur a au minimum un produit en stock a vendre.
@@ -228,9 +264,9 @@ public class Amizone {
     }
 
     /**
-     * Recherche parmis les utilisateurs du type oppose si au moins
-     * MIN_CATEGORIE_RECOMMAND categorie est en commun avec l'utilisateur en
-     * parametre.
+     * Methode utilitaire qui recherche parmis les utilisateurs du type oppose si
+     * au moins MIN_CATEGORIE_RECOMMAND categorie est en commun avec l'utilisateur
+     * en parametre.
      *
      * @param utilisateur Utilisateur qui recherche des profils comparables.
      * @return Liste des utilisateurs interessants ou liste vide si aucun
@@ -263,8 +299,8 @@ public class Amizone {
     }
 
     /**
-     * Compare les profils de deux Utilisateurs et retourne vrai si
-     * MIN_CATEGORIE_RECOMMAND categorie est en commun.
+     * Methode utilitaire qui compare les profils de deux Utilisateurs et retourne
+     * vrai si MIN_CATEGORIE_RECOMMAND categorie est en commun.                        !!!!!!!!!!!!!!!!!!!!!!!!!!! MIN_CATEGORIE_RECOMMAND sert a quoi?????
      *
      * @param user1 Utilisateur
      * @param user2
@@ -283,6 +319,25 @@ public class Amizone {
             }
         }
         return estComparable;
+    }
+    
+    /**
+     * Methode utilitaire qui recherche un mot cle à l'interieur de la description
+     * d'un produit.
+     * 
+     * @param produit Le produit pour la recherche
+     * @param motCle Le mot cle recherche
+     * @return true si trouve ou false sinon.
+     */
+    private boolean rechStringDansDescriptionProduit(Produit produit, String motCle) {
+        boolean trouve = false;
+        String descriptionProduit = produit.getDescription().toLowerCase();
+        motCle = motCle.toLowerCase();
+        
+        if (descriptionProduit.contains(motCle)) {
+            trouve = true;
+        }
+        return trouve;
     }
 
 }
